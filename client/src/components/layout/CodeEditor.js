fd477765faebbe039dfc5eb1af6085af38d93f37
joelmackenz <joelmackenzie3@gmail.com>
2021-04-30 10:13:20 -0700

    Merge branch 'dev' with past-interviews

--- client/src/components/layout/CodeEditor.js
+++ client/src/components/layout/CodeEditor.js
@@ -14,6 +14,18 @@ import "codemirror/addon/fold/foldgutter";
 import "codemirror/addon/fold/foldgutter.css";
 import "codemirror/addon/fold/brace-fold";
 
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import "codemirror/mode/clike/clike";
+import "codemirror/mode/python/python";
+
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/addon/fold/foldcode";
+import "codemirror/addon/fold/foldgutter";
+import "codemirror/addon/fold/foldgutter.css";
+import "codemirror/addon/fold/brace-fold";
+
 import { Controlled as ControlledEditor } from "react-codemirror2";
 
 import { makeStyles } from "@material-ui/core/styles";
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -14,6 +15,23 @@ ReactDOM.render(
   </AuthProvider>,
   document.getElementById("root")
 );
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import SocketProvider from './context/SocketContext'
+import AuthProvider from './context/AuthContext'
+
+ReactDOM.render(
+    <AuthProvider>
+        <SocketProvider>
+            <App />
+        </SocketProvider>
+    </AuthProvider>,
+    document.getElementById('root'));
+>>>>>>> 94a3b3a7b53dc84c757cfad3a706fccb3cf83124
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
